f1503e5a63b2cc9803e59539c0ae4647625c8bf4
swawa-yu <swawa.yu@gmail.com>
2024-10-17 09:20:12 +0900

    station4: useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 // DO NOT DELETE
 
 import './App.css'
+import { useState } from 'react'
 
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = useState('https://images.dog.ceo/breeds/greyhound-italian/n02091032_658.jpg')
   return (
     <div>
       <header>Hoge App</header>
       <p>犬の画像を表示するサイトです。</p>
-      <img src="https://images.dog.ceo/breeds/greyhound-italian/n02091032_658.jpg" alt="犬の画像" />
+      <img src={dogUrl} alt="犬の画像" />
     </div>
   )
 }
