77daa2f5420073c5ce4633415460113bd5f6dab4
swawa-yu <swawa.yu@gmail.com>
2024-10-17 09:18:22 +0900

    station3: add dog image and description

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ export const App = () => {
   return (
     <div>
       <header>Hoge App</header>
+      <p>犬の画像を表示するサイトです。</p>
+      <img src="https://images.dog.ceo/breeds/greyhound-italian/n02091032_658.jpg" alt="犬の画像" />
     </div>
   )
 }
