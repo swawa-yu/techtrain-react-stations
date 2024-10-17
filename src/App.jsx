5f4b9d3834a50b90193ab94d04d5911d788fdab8
swawa-yu <swawa.yu@gmail.com>
2024-10-17 09:22:56 +0900

    station5: change image when button clicked

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ export const App = () => {
       <header>Hoge App</header>
       <p>犬の画像を表示するサイトです。</p>
       <img src={dogUrl} alt="犬の画像" />
+      <button onClick={() => setDogUrl('https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg')}>更新</button>
     </div>
   )
 }
