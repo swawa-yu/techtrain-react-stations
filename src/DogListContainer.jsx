1078cf804129aedb4d271c20d8d1e12920efa349
swawa-yu <swawa.yu@gmail.com>
2024-10-17 12:24:48 +0900

    station9: useEffect

--- src/DogListContainer.jsx
+++ src/DogListContainer.jsx
@@ -1,7 +1,21 @@
 // @ts-check
 
+import { useState, useEffect } from 'react'
+
 export const DogListContainer = () => {
-  return <></>
+  const [breeds, setBreeds] = useState([''])
+
+  useEffect(() => {
+    const fetchBreeds = async () => {
+      const response = await fetch('https://dog.ceo/api/breeds/list/all')
+      const data = await response.json()
+      setBreeds(Object.keys(data.message))
+    }
+    fetchBreeds()
+  }, [])
+  return (<>
+
+  </>)
 }
 
 export default DogListContainer
